3053b2d5579040a03ae0d7ad1277eabb8fd948df
Pete Stewart <pete@newint.org>
2014-11-07 10:29:27 +0000

    Changed permisions and group

--- js/src/enhance.js
+++ js/src/enhance.js
@@ -1,31 +1,9 @@
-console.log('test');
-
-// window.addEvent('domready', function() {
-//     console.log('The DOM is ready!');
-//     var menu_buttons = $$('.menu-button');
-//     menu_buttons.addEvent('click', function(e) {
-//       e.stop();
-//       var t = this.get('href');
-//       var sc = 'selected';
-//       var tc = 'target';
-//       if(this.hasClass(sc)) {
-//         this.removeClass(sc);
-//         $$(t).removeClass('target');
-//       } else {
-//         menu_buttons.removeClass(sc);
-//         this.addClass(sc);
-//         $$('.holder').removeClass(tc);
-//         $$(t).addClass(tc);
-//       }
-//     });
-// });
-
-
 window.addEvent('domready', function() {
   var menu_buttons = $$('.menu-button');
   var close_buttons = $$('.close-btn');
   var sc = 'selected';
   menu_buttons.addEvent('click', function(e) {
+    history.go(-1);
     if(this.hasClass(sc)) {
       e.stop();
       closeMenu();
@@ -41,6 +19,7 @@ window.addEvent('domready', function() {
   });
 
   function closeMenu() {
+    history.go(-1);
     menu_buttons.removeClass(sc);
     window.location = "#nav-open-btn";
   }
