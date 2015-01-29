8ac54d0d220fe5bf64e552c5f7492798ef4ef4d6
Pete Stewart <pete@newint.org>
2015-01-29 11:33:00 +0000

    Worked on sharethis states and statemachine.js

--- js/src/sharethis.js
+++ js/src/sharethis.js
@@ -2,26 +2,23 @@
  *       Global Variables       *
  *******************************/
 var show_class = "showthis",
+    show_btn_class = "showbtn",
     transition_class = "transition",
+    right_icon = "fa-angle-double-right",
+    left_icon = "fa-angle-double-left",
     sharethis_class = ".sharethis_box",
-    loaded = false,
+    btn_class = ".toggle_sharethis",
+    $btn = $(btn_class),
     $st = $(sharethis_class),
+    loaded = false,
     usn_all,
     usn_medium,
     bpw,
+    fsm = new NI.FSM(),
     debug = true;
 
 $(document).ready(function() {
-
-    $st.on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(e){
-        e.stopPropagation();
-        dbug("transition ended");
-        if($st.hasClass(show_class)) {
-            dbug("Showing!!");
-        } else {
-            dbug("Not Showing!!");
-        }
-    });
+    $btn.on("click", toggleClick);
 });
 
 
@@ -33,49 +30,162 @@ $(document).ready(function() {
  * Functions
  */
 
-var toggleShareThis = function() {
+var shareThis_toggle = function() {
+    console.log("shareThis_toggle function");
     if(bpw > usn_medium) {
-        if(loaded) $st.addClass(show_class);
+        dbug("a");
+        fsm.transitionState("show");
     } else {
-        $st.removeClass(show_class);
+        dbug("b");
+        fsm.transitionState("hide");
     }
 }
 
-var hideShareThis = function() {
-    dbug("hideFunction");
-    $st.removeClass(show_class);
-}
-
-var showShareThis = function() {
-    if(loaded) $st.addClass(show_class);
-}
-
-var loadedShareThis = function() {
+var shareThis_loaded = function() {
     loaded = true;
     setTimeout(function() { // Set 1s delay on load as sharethis doesn't fire callback on load and look odd if not loaded.
         $st.addClass(transition_class);
-        toggleShareThis();
+        $btn.addClass(transition_class);
+        shareThis_toggle();
     }, 1000);
 }
+
+/**
+ * Static state function
+ */
+
+var switchBtnIcon = function(sh) {
+    // This function's a little obscure so I'll explain it.
+    // if you send it true it switches the button icon to right >> (close),
+    // and if false it switches it to left  << (open).
+    var $i = $("i", $btn);
+    sh ? $i.removeClass(left_icon).addClass(right_icon) : $i.removeClass(right_icon).addClass(left_icon);
+}
+
+/**
+ * Transition state functions
+ */
+
+var shareThis_showClosable = function() {
+    var state = fsm.getState();
+    fsm.listeners["transition-end"].call(this, $st, function($elm) {
+        if($btn.hasClass(show_btn_class)) {
+            fsm.setState("open-closable");
+            dbug(fsm.getState());
+        }
+        else {
+            fsm.listeners["transition-end"].call(this, $btn, function($elm) {
+                fsm.setState("open-closable");
+                dbug(fsm.getState());
+            });
+            $btn.addClass(show_btn_class);
+            switchBtnIcon(true);
+        }
+    });
+    if(loaded) $st.addClass(show_class);
+}
+
+var shareThis_hide = function() {
+    var state = fsm.getState();
+    if($st.hasClass(show_class)) {
+        fsm.listeners["transition-end"].call(this, $st, function($elm) {
+            if($btn.hasClass(show_btn_class)) {
+                fsm.setState("closed");
+                dbug(fsm.getState());
+            }
+            else {
+                fsm.listeners["transition-end"].call(this, $btn, function($elm) {
+                    fsm.setState("closed");
+                    dbug(fsm.getState());
+                });
+                $btn.addClass(show_btn_class);
+                switchBtnIcon(false);
+            }
+        });
+        $st.removeClass(show_class);
+    } else {
+        if($btn.hasClass(show_btn_class)) {
+            fsm.setState("closed");
+            dbug(fsm.getState());
+        }
+        else {
+            fsm.listeners["transition-end"].call(this, $btn, function($elm) {
+                fsm.setState("closed");
+                dbug(fsm.getState());
+            });
+            $btn.addClass(show_btn_class);
+            switchBtnIcon(false);
+        }
+    }
+
+}
+
+var shareThis_show = function() {
+    var state = fsm.getState();
+    fsm.listeners["transition-end"].call(this, $st, function($elm) {
+        if($btn.hasClass(show_btn_class)) {
+            fsm.listeners["transition-end"].call(this, $btn, function($elm) {
+                fsm.setState("open");
+                dbug(fsm.getState());
+            });
+            $btn.removeClass(show_btn_class);
+        } else {
+            fsm.setState("open");
+            dbug(fsm.getState());
+        }
+    });
+    if(loaded) $st.addClass(show_class);
+
+}
+
+
+/**
+ * Util functions
+ */
 
 var cleanPXString = function(str) {
     return Number(str.replace("px", ""));
 }
-
-
 
 var dbug = function(msg) {
     if(debug) console.log(msg);
 }
 
 /**
+ * Event function
+ */
+
+var toggleClick = function(e) {
+    var state = fsm.getState();
+    if(state != "closed") {
+        fsm.transitionState("hide");
+    } else {
+        fsm.transitionState("show-closable");
+    }
+    dbug("button clicked!");
+    dbug(state);
+}
+
+var transitionListener = function($elm, callback) {
+    $elm.on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(e){
+        e.stopPropagation();
+        dbug("transition ended");
+        $elm.off("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd");
+        callback.call(this, $elm);
+    });
+}
+
+
+
+/**
  * State Building
  */
 
-var fsm = new NI.FSM();
-fsm.extendStates({"open" : showShareThis, "close" : hideShareThis});
-fsm.changeState("open");
-dbug(fsm.states);
+//var fsm = new NI.FSM();
+//fsm.extendStates({ "open-closable": shareThis_openClosable, "closed": shareThis_closed, "open": shareThis_open});
+fsm.extendTransitions({ "show" : shareThis_show, "hide" : shareThis_hide, "show-closable" : shareThis_showClosable });
+fsm.extendListeners({ "transition-end" : transitionListener });
+//fsm.transitionState("show");
 
 /**
  * Event Binding
@@ -87,12 +197,12 @@ $( sharethis_class ).bind('DOMSubtreeModified', function(e) {
     var bp_now = Unison.fetch.now();
     usn_medium = cleanPXString(usn_all['usn-medium']);
     bpw = cleanPXString(bp_now.width);
-    loadedShareThis();
+    shareThis_loaded();
 });
 
 
 Unison.on('change', function(bp) {
     dbug("BreakPoint Changed to: " + bp.width);
     bpw = cleanPXString(bp.width);
-    toggleShareThis();
+    shareThis_toggle();
 });
--- js/src/statemachine.js
+++ js/src/statemachine.js
@@ -1,17 +1,24 @@
 var Fsm = function() {
+    "use strict";
     // Varibles
     var self = this;
     self.state = false;
-    self.states = {};
+    self.new_state = false;
+    self.prev_state = false;
+    self.current_state = false;
+    self.transitions = {};
+    self.listeners = {};
     // Functions
-    self.changeState = function(s, arg) {
-        if(typeof self.states[s] === "function") {
-            self.state = s;
+    self.transitionState = function(s, arg) {
+        if(typeof self.transitions[s] === "function") {
+            self.prev_state = self.state;
+            self.new_state = s;
+            self.state = { "transitioning" : { "from" : self.prev_state, "to" : self.new_state } };  
             if(typeof arg !== undefined) {
-                self.states[s].apply(self, arg);
+                self.transitions[s].apply(self, arg);
             }
             else {
-                self.states[s].call(self);
+                self.transitions[s].call(self);
             }
         } else {
             console.log("The doesn't seem to be a function set for " + s + " state.  Here is the states object:");
@@ -19,12 +26,20 @@ var Fsm = function() {
         }
     }
 
-    self.extendStates = function(obj) {
-        $.extend(self.states, obj);
+    self.extendTransitions = function(obj) {
+        $.extend(self.transitions, obj);
+    }
+
+    self.extendListeners = function(obj) {
+        $.extend(self.listeners, obj);
     }
 
     self.getState = function() {
         return self.state;
+    }
+
+    self.setState = function(state) {
+        self.state = state;
     }
 }
 
